94a5d9000c42b68e2a2fee8c175a56b0764a075b
Jonas Schmidt <jonaschmidt.en@gmail.com>
2023-01-08 21:35:19 -0600

    Braille Dictionary Converter
    All this was to generate braille_dicitonary.txt automatically from the table copied from https://en.wikipedia.org/wiki/Braille_ASCII

---
+++ main.py
@@ -0,0 +1,19 @@
+'''
+created by Jonas Schmidt on 1/8/2023
+'''
+
+# table = array of each entry of the table
+table_file = open("braille_table.txt", "r", errors="ignore")
+table = table_file.read().split("\t")
+
+table_file.close()
+
+dict = open("braille_dictionary.txt", "w")
+
+i = 1
+
+while i + 1 < len(table):
+    dict.writelines([table[i], " : ", table[i + 1], '\n'])
+    i += 5
+
+dict.close()
